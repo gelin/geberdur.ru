7ba55b730e9b7da1c5d17facec287b3868210ab6
Denis Nelubin <dnelubin@gmail.com>
2016-12-31 19:39:03 +0600

    Index now contains links to tales.

--- bin/index.py
+++ bin/index.py
@@ -1,16 +1,37 @@
 #!/usr/bin/env python3
 
 import os
-from mako.template import Template
+import os.path
+import codecs
 from mako.lookup import TemplateLookup
 from pypugjs.ext.mako import preprocessor as pug_preprocessor
+import hoep
 
 
-def tales_list():
-    for f in filter(lambda fname: fname.endswith('.md'),
+class HeaderCatcher(hoep.Hoep):
+
+    def __init__(self, extensions=0, render_flags=0):
+        super(HeaderCatcher, self).__init__(extensions, render_flags)
+        self.first_head = None
+
+    def header(self, text, level):
+        if self.first_head is None and level == 1:
+            self.first_head = text
+        return ''
+
+
+def list_tales():
+    for mdfile in filter(lambda f: f.endswith('.md'),
                     sorted(os.listdir('src/tales'))):
-        pass
-
+        tale = {
+            'link': mdfile.split('-', maxsplit=1)[1].rsplit('.', maxsplit=1)[0]
+        }
+        with codecs.open(os.path.join('src/tales', mdfile), mode='r', encoding='utf-8') as md:
+            text = md.read()
+            catcher = HeaderCatcher()
+            catcher.render(text)
+            tale['title'] = catcher.first_head
+        yield tale
 
 
 def main():
@@ -18,7 +39,9 @@ def main():
         lookup = TemplateLookup(directories=['src/templates'],
                                 input_encoding='utf-8',
                                 preprocessor=pug_preprocessor)
-        f.write(lookup.get_template('index.pug').render())
+        f.write(lookup.get_template('index.pug').render(
+            tales=list(list_tales())
+        ))
 
 
 if __name__ == '__main__':
