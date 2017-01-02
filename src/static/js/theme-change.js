6eb82083d93635cff641d587a44db46dbc5a53a4
Denis Nelubin <dnelubin@gmail.com>
2017-01-02 21:01:41 +0600

    Added ability to change font size (and it's saved to local storage).

--- src/static/js/theme-change.js
+++ src/static/js/theme-change.js
@@ -22,10 +22,12 @@
     }
   }
 
-  if (supports_storage) {
-    var theme = localStorage.theme;
-    if (theme) {
-      set_theme(theme);
+  function set_font_size(size) {
+    console.log('set_font_size: ' + size);
+    $('p').css('font-size', parseInt(size));
+    $('a.list-group-item').css('font-size', parseInt(size));
+    if (supports_storage) {
+      localStorage.fontSize = parseInt(size);
     }
   }
 
@@ -37,15 +39,50 @@
       next_index = 0;
     }
     set_theme(themes[next_index]);
+    return false;
+  }
+
+  function increase_font_size() {
+    var current_size = parseInt($('p').first().css('font-size'));
+    if (current_size >= 200) {
+      set_font_size(200);
+    } else {
+      set_font_size(current_size + 2);
+    }
+    return false;
+  }
+
+  function decrease_font_size() {
+    var current_size = parseInt($('p').first().css('font-size'));
+    if (current_size <= 8) {
+      set_font_size(8);
+    } else {
+      set_font_size(current_size - 2);
+    }
+    return false;
   }
 
   $(function() {
     if (supports_storage) {
-      $('#theme-switch').click(function() {
-        switch_theme();
-      });
+
+      var theme = localStorage.theme;
+      if (theme) {
+        set_theme(theme);
+      }
+
+      var fontSize = localStorage.fontSize;
+      if (fontSize) {
+        set_font_size(fontSize);
+      }
+
+      $('#theme-switch').click(switch_theme);
+      $('#font-size-up').click(increase_font_size);
+      $('#font-size-down').click(decrease_font_size);
+
     } else {
-      $('#theme-switch').hide();
+
+      $('#theme-buttons').hide();
+
     }
   });
 
