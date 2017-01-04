a211fbad19cce8648cbd938ed0e048c47e8a8f0d
Denis Nelubin <dnelubin@gmail.com>
2017-01-04 17:18:22 +0600

    Small optimization to avoid reading of all tale files.

--- bin/tales.py
+++ bin/tales.py
@@ -11,20 +11,22 @@ from libtales import tale_link, Tales, TALE_PATTERN
 
 
 def render_tale(talefile, tales):
-    tale = tale_link(talefile)
-    print('Building tale/{}/index.html'.format(tale))
-    os.makedirs('build/tale/{}'.format(tale), exist_ok=True)
-    with open('build/tale/{}/index.html'.format(tale), 'wt') as htmlfile:
+    talelink = tale_link(talefile)
+    print('Building tale/{}/index.html'.format(talelink))
+    os.makedirs('build/tale/{}'.format(talelink), exist_ok=True)
+    with open('build/tale/{}/index.html'.format(talelink), 'wt') as htmlfile:
         lookup = TemplateLookup(directories=['src/templates'],
                                 input_encoding='utf-8',
                                 preprocessor=pug_preprocessor)
+        tale = tales[talelink]
+        tale.find_title()
         htmlfile.write(lookup.get_template('tale.pug').render(
-            tale=tales[tale]
+            tale=tale
         ))
 
 
 def main(args):
-    tales = Tales(read_markdown=True)
+    tales = Tales()
     for talefile in filter(TALE_PATTERN.match, args):
         render_tale(talefile, tales)
 
